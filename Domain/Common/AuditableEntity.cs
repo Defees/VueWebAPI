bdd63720c3fe62663fc81ea28cd7b81764ef77b5
Ivan_Bartov <vanya.bartov@gmail.com>
2022-09-30 18:38:22 +0300

    Changed AuditableEntity

--- Domain/Common/AuditableEntity.cs
+++ Domain/Common/AuditableEntity.cs
@@ -4,8 +4,8 @@ namespace Domain.Common;
 
 public class AuditableEntity : Entity
 {
-    public DateTime Created { get; set; }
-    public string CreatedBy { get; set; }
+    public DateTime? Created { get; set; }
+    public string? CreatedBy { get; set; }
     public DateTime? LastModified { get; set; }
-    public string LastModifiedBy { get; set; }
+    public string? LastModifiedBy { get; set; }
 }
